3997ffa54412202fac71af8f4ed72e1859be6beb
Mistsu <vietanhtran12@gmail.com>
2022-10-05 23:48:35 +0700

    Update validation process

--- src/actions/auth.action.js
+++ src/actions/auth.action.js
@@ -1,66 +1,87 @@
 
 import StringFormat from "string-format";
+import { object, string, boolean, array } from "yup";
 import { AuthApi } from "api";
 import { TxtConstant } from "const";
 import { objCheckIfKeyExists } from "utils/data.util";
 
 async function authLogin(data) {
-  var response = await AuthApi.login({
-    userID: data.username,
-    deviceID: "to-be-filled", // TODO: filling this device id?
-    password: data.password,
+  // Create schemas
+  let requestSchema = object({
+    userID: string().required(),
+    deviceID: string().required(),
+    password: string().required(),
   });
 
-  var loginOK = false;
-  var error = null;
+  let responseSchema = object({
+    success: boolean().required(),
+    error: string().required().default(""),
+  });
 
+  // Send data
+  var request = requestSchema.cast({
+                  userID: data.username,
+                  deviceID: data.deviceID,
+                  password: data.password,
+                });
+  var response = await AuthApi.login(request);
+
+  // Check output
+  var success = false;
+  var error = "";
   if (response.ok) {
-    var data = response.data;
-    if (objCheckIfKeyExists(data, ["success", "error"])) {
-      if (!data.success)
-        error = data.error;
-      else
-        loginOK = true;
-    } else {
+    try {
+      await responseSchema.validate(response.data);
+    } catch (err) {
       error = StringFormat(TxtConstant.FM_REQUEST_ERROR, TxtConstant.ERR_INVALID_RESPONSE_FROM_SERVER);
     }
   } else {
     error = StringFormat(TxtConstant.FM_REQUEST_ERROR, response.problem);
   }
 
-  return {
-    success: loginOK,
-    error: error
-  }
+  return responseSchema.cast({
+    success: success,
+    error: error,
+  });
 };
 
 async function authRegister(data) {
-  var response = await AuthApi.register({
-    userID: data.username,
-    password: data.password,
+  // Create schemas
+  let requestSchema = object({
+    userID: string().required(),
+    password: string().required(),
   });
 
-  var registerOK = false;
-  var error = null;
+  let responseSchema = object({
+    success: boolean().required(),
+    success: string().default(),
+  });
 
+  // Send data
+  var request = requestSchema.cast({
+                  userID: data.username,
+                  password: data.password,
+                });
+  var response = await AuthApi.login(request);
+
+  // Check output
+  var success = false;
+  var error = "";
   if (response.ok) {
-    var data = response.data;
-    if (objCheckIfKeyExists(data, ["success", "error"])) {
-      if (!data.success)
-        error = data.error;
-      else
-        registerOK = true;
-    } else {
+    try {
+      var data = await responseSchema.validate(response.data);
+      return data;
+    } catch (err) {
       error = StringFormat(TxtConstant.FM_REQUEST_ERROR, TxtConstant.ERR_INVALID_RESPONSE_FROM_SERVER);
     }
   } else {
     error = StringFormat(TxtConstant.FM_REQUEST_ERROR, response.problem);
   }
 
-  return {
-    success: registerOK,
-    error: error
-  }
+  return responseSchema.cast({
+    success: success,
+    error: error,
+  });
 };
 
 export {
