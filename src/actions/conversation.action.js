c8fbe04680f8e94ad29fcff4c2f85926d1233b46
Mistsu <vietanhtran12@gmail.com>
2022-10-12 01:14:54 +0700

    add

--- src/actions/conversation.action.js
+++ src/actions/conversation.action.js
@@ -72,8 +72,9 @@ function encryptIntialMessageNewSession(userID, deviceID, NativeBobPrekeyBundle)
     var ciphertext = CryptoInteractor.innerEncrypt(sharedSecret, associatedData, associatedData);
     console.log(bufferToHex(associatedData));
     console.log(bufferToHex(ciphertext));
-    console.log(CryptoInteractor.innerDecrypt(sharedSecret, associatedData, associatedData));
-    console.log(CryptoInteractor.innerDecrypt(sharedSecret, ciphertext, associatedData));
+    console.log(bufferToHex(sharedSecret));
+    console.log(bufferToHex(CryptoInteractor.innerDecrypt(sharedSecret, associatedData, associatedData)));
+    console.log(bufferToHex(CryptoInteractor.innerDecrypt(sharedSecret, ciphertext, associatedData)));
     console.log("...")
     console.log("...")
     console.log("...")
