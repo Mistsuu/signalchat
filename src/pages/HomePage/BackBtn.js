df79b5a1fb8e4dc854581c01a0ae35cc1cb43d9c
Mistsu <vietanhtran12@gmail.com>
2022-10-12 13:19:28 +0700

    Update back button

--- src/pages/HomePage/BackBtn.js
+++ src/pages/HomePage/BackBtn.js
@@ -2,16 +2,17 @@ import React, { memo } from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@mui/styles";
 import { Box } from "@mui/material";
+import WestIcon from '@mui/icons-material/West';
 import { Button } from "components";
-import { fontWeight } from "@mui/system";
+
 
 const BackBtn = ({ onClick, ...otherProps }) => {
   const classes = useStyles();
 
   return (
-    <Button width={70} className={classes.backBtn} onClick={onClick}>
+    <Button width={70} height={30} className={classes.backBtn} onClick={onClick}>
       <Box>
-        Back
+        <WestIcon/>
       </Box>
     </Button>
   )
@@ -28,7 +29,7 @@ export default memo(BackBtn);
 const useStyles = makeStyles(theme => ({
   backBtn: {
     position: "absolute",
-    top: 64,
+    top: 65,
     left: 10,
     borderRadius: 20,
     borderWidth: 1,
